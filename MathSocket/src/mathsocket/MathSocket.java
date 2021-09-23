c7bf5732f48d42629d44554150b515016dfae305
luisale06 <86024351+luisale06@users.noreply.github.com>
2021-09-23 00:07:19 -0600

    Update MathSocket.java

--- MathSocket/src/mathsocket/MathSocket.java
+++ MathSocket/src/mathsocket/MathSocket.java
@@ -1,9 +1,3 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mathsocket;
 
 import java.awt.event.ActionEvent;
@@ -13,10 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Estudiante
- */
 public class MathSocket {
 
     int mov;
