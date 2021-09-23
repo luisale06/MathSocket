e4a25db31f79d67f0ebd47ed57440422b66b45e4
luisale06 <86024351+luisale06@users.noreply.github.com>
2021-09-22 22:44:52 -0600

    Update Avatar.java

--- MathSocket/src/mathsocket/Avatar.java
+++ MathSocket/src/mathsocket/Avatar.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package mathsocket;
+
+/**
+ * @author Pablo
+ * @author Alejandro
+ */
 
 import java.awt.Graphics2D;
 
-/**
- *
- * @author Estudiante
- */
+
+
 public class Avatar {
 
     public int x = 45;
